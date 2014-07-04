0019872747095e66d9fa1ea344d7ae1e34673346
andreij6 <andreij6@gmail.com>
2014-07-03 20:02:10 -0500

    Can find 2 word Cities

--- Maps.Web/JavaScripts/Controllers/HomeCtrl.js
+++ Maps.Web/JavaScripts/Controllers/HomeCtrl.js
@@ -1,14 +1,38 @@
 ﻿todoApp.controller('HomeCtrl', function ($scope, IData) {
     $scope.todoItem;
-    
+
     $scope.processItem = function () {
         var arrayOfWords = $scope.todoItem.split(" ");
 
         $.each(arrayOfWords, function (index, value) {
-            if(IData.Cities.indexOf(value) !== -1)
-            {
-                console.log(arrayOfWords[index]);
+            var potentialCity;
+            //--ToDo
+            //Punctuation
+            //multi word Cities
+            // Case sensitivity
+
+            if (value[0] === value[0].toUpperCase()) {
+
+                if (index !== (arrayOfWords.length - 1))
+                {
+                    if(arrayOfWords[index + 1][0] == arrayOfWords[index + 1][0].toUpperCase())
+                    {
+                        potentialCity = value + " " + arrayOfWords[index + 1];
+
+                        if (IData.Cities.indexOf(potentialCity) !== -1)
+                        {
+                            console.log(potentialCity);
+                        }
+                    }
+                }
+
+                if (IData.Cities.indexOf(value) !== -1) {
+
+                    console.log(arrayOfWords[index]);
+                }
+
             }
+
         });
 
     }
